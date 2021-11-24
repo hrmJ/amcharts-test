9e7b40677b91a2c67436c3e3336abf8422ca2c03
Juho Härme <juho.harme@knowit.fi>
2021-11-24 11:07:03 +0200

    add routes etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
       <nav>
-        <ul>
-          <li>Line chart</li>
-        </ul>
+        <Link to="/">Home</Link>
+        <Link to="line">Line chart</Link>
       </nav>
       <main className="main"></main>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LineChart } from "./LineChart";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/line" element={<LineChart />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
