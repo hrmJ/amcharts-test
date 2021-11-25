0616831369ce00013619d3138b3e15edd273e2bb
Juho Härme <juho.harme@knowit.fi>
2021-11-25 12:52:42 +0200

    a working bar example with covid data

--- src/CovTest1.tsx
+++ src/CovTest1.tsx
@@ -9,22 +9,24 @@ am4core.useTheme(am4themes_animated);
 export const CovTest1: FC = function () {
   const chartRef = useRef<null | am4charts.XYChart>(null);
   useLayoutEffect(() => {
-    const chart = am4core.create("cov-case-chart", am4charts.XYChart);
-    chart.data = [
-      {
-        country: "Lithuania",
-        research: 3,
-      },
-    ];
-    const categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
-    categoryAxis.dataFields.category = "country";
-    chart.yAxes.push(new am4charts.ValueAxis());
-    const series = chart.series.push(new am4charts.ColumnSeries());
-    series.dataFields.valueY = "research";
-    series.dataFields.categoryX = "country";
-    chartRef.current = chart;
+    fetch(`http://localhost:3001/sample`)
+      .then((raw) => raw.json())
+      .then((data: CovCaseData[]) => {
+        const chart = am4core.create("cov-case-chart", am4charts.XYChart);
+        chart.data = data;
+        const x = chart.xAxes.push(new am4charts.CategoryAxis());
+        const y = chart.yAxes.push(new am4charts.ValueAxis());
+        x.dataFields.category = "hcdmunicipality2020";
+        x.title.text = "Alue";
+        y.title.text = "Tapauksia";
+        const series = chart.series.push(new am4charts.ColumnSeries());
+        series.dataFields.categoryX = "hcdmunicipality2020";
+        series.dataFields.valueY = "value";
+        // series.columns.template.fill = am4core.color("#000000");
+        chartRef.current = chart;
+      });
     return () => {
-      chart.dispose();
+      chartRef.current?.dispose();
     };
   }, []);
 
