7f6bcbff59fed7f90747d0346b37765a7744b651
Juho Härme <juho.harme@knowit.fi>
2021-11-25 14:20:17 +0200

    mutate covid data before plotting

--- src/CovTest1.tsx
+++ src/CovTest1.tsx
@@ -2,28 +2,52 @@ import * as am4charts from "@amcharts/amcharts4/charts";
 import * as am4core from "@amcharts/amcharts4/core";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import { CovCaseData } from "amcharts-test-types";
-import { FC, useLayoutEffect, useRef } from "react";
+import { FC, useEffect, useLayoutEffect, useRef, useState } from "react";
 
 am4core.useTheme(am4themes_animated);
 
+type groupedVals = {
+  [key: string]: number;
+};
+
 export const CovTest1: FC = function () {
   const chartRef = useRef<null | am4charts.XYChart>(null);
+  const [isLoading, setIsloading] = useState(true);
   useLayoutEffect(() => {
+    setIsloading(true);
     fetch(`http://localhost:3001/sample`)
       .then((raw) => raw.json())
       .then((data: CovCaseData[]) => {
         const chart = am4core.create("cov-case-chart", am4charts.XYChart);
-        chart.data = data;
-        const x = chart.xAxes.push(new am4charts.CategoryAxis());
-        const y = chart.yAxes.push(new am4charts.ValueAxis());
-        x.dataFields.category = "hcdmunicipality2020";
-        x.title.text = "Alue";
-        y.title.text = "Tapauksia";
+        chart.data = data.filter(
+          (row) => row.hcdmunicipality2020 !== "Kaikki Alueet"
+        );
+        const grouped = data.reduce(
+          (groups: groupedVals, { hcdmunicipality2020, value }) => {
+            const valueAsNumber = value ?? "0";
+            const aggregated =
+              groups[hcdmunicipality2020] + parseInt(valueAsNumber) ||
+              parseInt(valueAsNumber);
+            return { ...groups, [hcdmunicipality2020]: aggregated };
+          },
+          {}
+        );
+        chart.data = Object.entries(grouped).map(
+          ([hcdmunicipality2020, value]) => ({ hcdmunicipality2020, value })
+        );
+        const categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+        const valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+        categoryAxis.dataFields.category = "hcdmunicipality2020";
+        categoryAxis.title.text = "Alue";
+        categoryAxis.renderer.minGridDistance = 20;
+        valueAxis.title.text = "Tapauksia";
         const series = chart.series.push(new am4charts.ColumnSeries());
-        series.dataFields.categoryX = "hcdmunicipality2020";
-        series.dataFields.valueY = "value";
-        // series.columns.template.fill = am4core.color("#000000");
+        series.dataFields.categoryY = "hcdmunicipality2020";
+        series.dataFields.valueX = "value";
+        series.columns.template.tooltipText = "{categoryY}: {valueX}";
+        series.groupFields.valueX = "sum";
         chartRef.current = chart;
+        setIsloading(false);
       });
     return () => {
       chartRef.current?.dispose();
@@ -32,7 +56,8 @@ export const CovTest1: FC = function () {
 
   return (
     <>
-      <div id="cov-case-chart" style={{ width: "100%", height: "500px" }}></div>
+      {isLoading && <p>Ladataaan...</p>}
+      <div id="cov-case-chart" style={{ width: "100%", height: "700px" }}></div>
     </>
   );
 };
