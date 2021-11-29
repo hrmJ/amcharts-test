4cd0b0ea9c111aa98015fec455d8a2a51c783f85
Juho Härme <juho.harme@knowit.fi>
2021-11-29 10:57:35 +0200

    move chart rendering to separate component

--- src/CovidCharts/Weekly/Chart.tsx
+++ src/CovidCharts/Weekly/Chart.tsx
@@ -1,6 +1,8 @@
 import * as am4charts from "@amcharts/amcharts4/charts";
+import * as am4core from "@amcharts/amcharts4/core";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import { CovCaseData } from "amcharts-test-types";
-import { FC, useEffect, useLayoutEffect, useRef, useState } from "react";
+import { FC, useEffect, useRef } from "react";
 import {
   addCategoryDataToAxis,
   addCurrentWeekRange,
@@ -10,17 +12,20 @@ import {
   createChart,
 } from "./chartUtils";
 
+am4core.useTheme(am4themes_animated);
+
 export interface CovChartWeeklyProps {
   data: CovCaseData[];
   currentRegions: { [key: string]: string };
+  setIsLoading: (val: boolean) => void;
 }
 
 export const CovChartWeekly: FC<CovChartWeeklyProps> = function ({
   data,
   currentRegions,
+  setIsLoading,
 }) {
   const chartRef = useRef<null | am4charts.XYChart>(null);
-  const [isLoading, setIsloading] = useState(true);
   useEffect(() => {
     const chart = createChart();
     const categoryAxis = createCategoryAxis(chart);
@@ -33,15 +38,14 @@ export const CovChartWeekly: FC<CovChartWeeklyProps> = function ({
     addScrollbar(chart);
     chart.cursor = new am4charts.XYCursor();
     chartRef.current = chart;
-    setIsloading(false);
+    console.log("SET");
+    setIsLoading(false);
     return () => {
       chartRef.current?.dispose();
     };
   }, [data, currentRegions]);
 
-  return isLoading ? (
-    <p>Ladataaan grafiikkaa...</p>
-  ) : (
+  return (
     <div id="cov-case-chart" style={{ width: "100%", height: "700px" }}></div>
   );
 };
--- src/CovidCharts/Weekly/ChartContainer.tsx
+++ src/CovidCharts/Weekly/ChartContainer.tsx
@@ -1,19 +1,15 @@
-import * as am4charts from "@amcharts/amcharts4/charts";
-import * as am4core from "@amcharts/amcharts4/core";
-import am4themes_animated from "@amcharts/amcharts4/themes/animated";
-import { FC, useRef, useState } from "react";
+import { FC, useState } from "react";
 import { useSelector } from "react-redux";
 import { loadingState } from "../../features/covidData/covidDataSlice";
 import { RegionSelect } from "../../RegionSelect";
 import { RootState } from "../../store";
 import { CovChartWeekly } from "./Chart";
 
-am4core.useTheme(am4themes_animated);
-
 export const CovChartWeeklyContainer: FC = function () {
   const [currentRegions, setRegions] = useState<{ [key: string]: string }>({
     compRegion1: "Kaikki Alueet",
   });
+  const [graphLoading, setGraphLoading] = useState(true);
   const { allData: data, loading: dataLoading } = useSelector(
     (state: RootState) => state.covidData
   );
@@ -47,7 +43,12 @@ export const CovChartWeeklyContainer: FC = function () {
         Lisää alue vertailuun
       </button>
       {dataLoading === loadingState.LOADING && <p>Ladataan dataa...</p>}
-      <CovChartWeekly currentRegions={currentRegions} data={data} />
+      {graphLoading && <p>Ladataan grafiikkaa...</p>}
+      <CovChartWeekly
+        currentRegions={currentRegions}
+        data={data}
+        setIsLoading={setGraphLoading}
+      />
     </>
   );
 };
